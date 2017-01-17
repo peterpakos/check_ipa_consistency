bff194859909277c62197e32df5310334a4f59c1
Martin <martin.basti@gmail.com>
2017-01-17 20:19:16 +0100

    add setup.py

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ It can also be used as a Nagios/Opsview plug-in (check -n, -w and -c  options).
 
 The tool has been tested in FreeIPA 4.2/4.3/4.4 (Centos 7.2/7.3, Fedora 24) environments.
 
-Requirements:
+## Requirements:
 * FreeIPA 4.2 or higher
-* Bash 4.0 or higher
+* Python2.7 or Python3.5+
+* python modules: pyYAML, tabular, six, pyldap
+
+## Installation:
+```
+python setup.py install [--user]
+```
 
 Any comments and improvement ideas are welcome.
 
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# Authors:
+#   Martin Basti <mbasti@redhat.com>
+#
+# Copyright (C) 2017  Peter Pakos <peter.pakos@wandisco.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(
+    name="ipa-check-consistency",
+    version="17.1.17",
+    packages=['ipa_consistency_checker'],
+
+    scripts=['ipa-check-consistency'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=[
+        'pyldap',
+        'pyaml',
+        'six',
+        'tabulate',
+    ],
+
+    # metadata for upload to PyPI
+    author="Peter Pakos",
+    author_email="peter.pakos@wandisco.com",
+    description="Checks consistency across FreeIPA servers",
+    license="GPL3+",
+    keywords="freeipa check consistency ldap",
+    url="https://github.com/peterpakos/ipa_check_consistency",
+)
